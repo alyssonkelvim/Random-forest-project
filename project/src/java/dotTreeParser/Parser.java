a0b588fd7c80a6fb86230774ff18c58b1caaf57f
Alysson Kelvim <akcsalysson@gmail.com>
2023-07-08 11:13:57 -0300

    Ajustar Parser

--- project/src/java/dotTreeParser/Parser.java
+++ project/src/java/dotTreeParser/Parser.java
@@ -1,61 +1,53 @@
-package project.src.java.dotTreeParser;
+package com.mycompany.randomforest.parser;
 
+import com.mycompany.randomforest.model.Tree;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import project.src.java.dotTreeParser.treeStructure.Tree;
-import project.src.java.dotTreeParser.treeStructure.TreeBuilder;
+
 
 public class Parser {
 
     public static List<String> featuresNames;
     public static Set<String> classesNames;
+    public static int samplesQuantity = 0;;
 
     public static List<Tree> execute(String dataset) throws IOException {
+    	System.out.println("Reading dataset...");
         readDatasetHeader(dataset);
+        System.out.println("Reading trees...");
         return readDatasetSamples(dataset);
     }
 
-    public static int getClassQuantity(){
-        return classesNames.size();
-    }
-
-    public static int getFeatureQuantity(){
-
-        /* the number of feature also count the class column, subtracting 1 to return the correct value */
-
-        return featuresNames.size() - 1;
-    }
-
     private static void readDatasetHeader(String dataset) throws IOException {
-
-        var path = System.getProperty("user.dir") + "/project/assets/datasets/" + dataset + ".csv";
+        var path = System.getProperty("user.dir") + "/assets/datasets/" + dataset + ".csv";
         var scanner = new Scanner(new File(path));
         var line = scanner.nextLine().split(",");
-
         featuresNames = Arrays.asList(line);
         classesNames = new HashSet<String>();
-
         while(scanner.hasNext()){
             line = scanner.nextLine().split(",");
-            classesNames.add(line[line.length - 1]);
+            classesNames.add(line[line.length-1]);
+            samplesQuantity++;
         }
 
         scanner.close();
     }
 
     private static List<Tree> readDatasetSamples(String dataset) throws IOException {
-        var path = System.getProperty("user.dir") + "/project/assets/trees/" + dataset;
+        var path = System.getProperty("user.dir") + "/assets/trees/" + dataset;
         var files = listFiles(path);
         return files
             .stream()
-            .sorted()
             .map(file -> parseFromDot(path, file))
             .collect(Collectors.toList());
-
     }
 
     private static Tree parseFromDot(String path, String file){
@@ -68,7 +60,8 @@ public class Parser {
     }
 
     private static Set<String> listFiles(String path) {
-        return Stream.of(new File(path).listFiles())
+    	var files = new File(path).listFiles();
+        return Stream.of(files)
           .filter(file -> !file.isDirectory())
           .map(File::getName)
           .collect(Collectors.toSet());
